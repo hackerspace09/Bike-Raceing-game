3295477bce2080ad2ba26179e46148f40aa28943
hackerspace09 <46839090+hackerspace09@users.noreply.github.com>
2021-01-11 19:50:19 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var bikes, bike1, bike2;
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  bike1_img = loadImage("images/bike1.png");
+  bike2_img = loadImage("images/bike2.png");
+  ground = loadImage("images/ground.png");
+
+
+
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end()
+  }
+}
